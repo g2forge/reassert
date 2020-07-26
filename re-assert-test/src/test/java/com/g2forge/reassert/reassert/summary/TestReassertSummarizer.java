6fe84551fa756deab108b5dbfccbada942ab3c67
Greg Gibeling <gdgib@outlook.com>
2020-07-26 14:23:09 -0700

    G2-878 Summarizer test infrastructure

--- re-assert-test/src/test/java/com/g2forge/reassert/reassert/summary/TestReassertSummarizer.java
+++ re-assert-test/src/test/java/com/g2forge/reassert/reassert/summary/TestReassertSummarizer.java
@@ -6,22 +6,50 @@ import org.slf4j.event.Level;
 import com.g2forge.alexandria.java.core.resource.Resource;
 import com.g2forge.alexandria.java.io.dataaccess.ByteArrayDataSink;
 import com.g2forge.alexandria.test.HAssert;
+import com.g2forge.reassert.core.model.artifact.Artifact;
+import com.g2forge.reassert.core.model.report.IReport;
+import com.g2forge.reassert.list.ListCoordinates;
 import com.g2forge.reassert.mock.MockCoordinates;
+import com.g2forge.reassert.reassert.ATestReassert;
 import com.g2forge.reassert.reassert.ReassertContext;
-import com.g2forge.reassert.reassert.summary.ReassertSummarizer;
+import com.g2forge.reassert.reassert.algorithm.example.ExampleGraph;
 import com.g2forge.reassert.reassert.summary.model.ArtifactSummary;
 import com.g2forge.reassert.reassert.summary.model.ReportSummary;
-import com.g2forge.reassert.reassert.summary.model.ReportSummary.ReportSummaryBuilder;
 
-public class TestReassertSummarizer {
+import lombok.AccessLevel;
+import lombok.Getter;
+
+public class TestReassertSummarizer extends ATestReassert {
+	@Getter(value = AccessLevel.PROTECTED, lazy = true)
+	private final ReassertSummarizer summarizer = new ReassertSummarizer(ReassertContext.getContext());
+
 	@Test
 	public void artifacts() {
-		final ReportSummaryBuilder summary = ReportSummary.builder();
+		final ReportSummary.ReportSummaryBuilder summary = ReportSummary.builder();
 		summary.artifact(ArtifactSummary.builder().level(Level.WARN).artifact(new MockCoordinates("A")).finding(null).build());
 		summary.artifact(ArtifactSummary.builder().level(Level.ERROR).artifact(new MockCoordinates("B")).finding(null).build());
+		assertOutput("artifacts", summary.build());
+	}
 
+	protected void assertOutput(final String name, final ReportSummary summary) {
 		final ByteArrayDataSink sink = new ByteArrayDataSink();
-		new ReassertSummarizer(ReassertContext.getContext()).renderArtifacts(summary.build(), sink);
-		HAssert.assertEquals(new Resource(getClass(), "artifacts.csv").read(false), sink.getStream().toString());
+		getSummarizer().renderArtifacts(summary, sink);
+		HAssert.assertEquals(new Resource(getClass(), name + "-output.csv").read(false), sink.getStream().toString());
+	}
+
+	@Override
+	protected ExampleGraph load(Artifact<ListCoordinates> artifact) {
+		return new ExampleGraph(artifact);
+	}
+
+	@Test
+	public void summary() {
+		test("summary");
+	}
+
+	protected void test(final String name) {
+		final IReport report = load(name).getReport();
+		final ReportSummary summary = getSummarizer().summarize(report);
+		assertOutput(name, summary);
 	}
 }
--- re-assert/src/main/java/com/g2forge/reassert/reassert/summary/ReassertSummarizer.java
+++ re-assert/src/main/java/com/g2forge/reassert/reassert/summary/ReassertSummarizer.java
@@ -2,15 +2,21 @@ package com.g2forge.reassert.reassert.summary;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.g2forge.alexandria.java.core.helpers.HCollection;
 import com.g2forge.alexandria.java.io.RuntimeIOException;
 import com.g2forge.alexandria.java.io.dataaccess.IDataSink;
+import com.g2forge.alexandria.java.type.ref.ATypeRef;
 import com.g2forge.alexandria.java.type.ref.ITypeRef;
 import com.g2forge.reassert.core.algorithm.ReassertVertexDescriber;
 import com.g2forge.reassert.core.api.module.IContext;
+import com.g2forge.reassert.core.model.artifact.Artifact;
 import com.g2forge.reassert.core.model.report.IReport;
 import com.g2forge.reassert.reassert.summary.convert.SummaryModule;
 import com.g2forge.reassert.reassert.summary.model.ArtifactSummary;
@@ -23,12 +29,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReassertSummarizer {
 	protected final IContext context;
-	
+
 	public void renderArtifacts(ReportSummary reportSummary, IDataSink sink) {
 		final CsvMapper mapper = new CsvMapper();
 		mapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
 		mapper.registerModule(new SummaryModule(new ReassertVertexDescriber(getContext())));
-		
+
 		final ObjectWriter writer = mapper.writerFor(ArtifactSummary.class).with(mapper.schemaFor(ArtifactSummary.class).withHeader().withColumnReordering(true));
 		try (final OutputStream stream = sink.getStream(ITypeRef.of(OutputStream.class))) {
 			writer.writeValues(stream).writeAll(reportSummary.getArtifacts());
@@ -38,6 +44,18 @@ public class ReassertSummarizer {
 	}
 
 	public ReportSummary summarize(IReport report) {
+		final List<Artifact<?>> artifacts = report.getGraph().vertexSet().stream().flatMap(new ATypeRef<Artifact<?>>() {}::castIfInstance).collect(Collectors.toList());
+		HCollection.sort(artifacts, new Comparator<Artifact<?>>() {
+
+			@Override
+			public int compare(Artifact<?> o1, Artifact<?> o2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		});
+		for (Artifact<?> artifact : artifacts) {
+
+		}
 		return null;
 	}
 }
