0af4945a43cc990766a1485f04486068bea6bf8d
Greg Gibeling <gdgib@outlook.com>
2020-08-22 07:51:07 -0700

    G2-899 Remove out-dated CSV code from re-list

--- re-list/src/main/java/com/g2forge/reassert/list/AListRepository.java
+++ re-list/src/main/java/com/g2forge/reassert/list/AListRepository.java
@@ -1,13 +1,5 @@
 package com.g2forge.reassert.list;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.g2forge.alexandria.java.io.RuntimeIOException;
-import com.g2forge.alexandria.java.type.ref.ITypeRef;
 import com.g2forge.reassert.core.api.IReassertGraphBuilder;
 import com.g2forge.reassert.core.api.system.ARepository;
 import com.g2forge.reassert.core.model.artifact.Artifact;
@@ -23,16 +15,6 @@ import lombok.ToString;
 @Getter(AccessLevel.PROTECTED)
 @RequiredArgsConstructor
 public abstract class AListRepository extends ARepository<ListCoordinates, ListSystem> {
-	protected static <Entry> List<Entry> read(ListCoordinates coordinates, final Class<Entry> type) {
-		final CsvMapper mapper = new CsvMapper();
-		final ObjectReader reader = mapper.readerFor(type).with(mapper.schemaFor(type).withHeader().withColumnReordering(true));
-		try (final InputStream stream = coordinates.getSource().getStream(ITypeRef.of(InputStream.class))) {
-			return reader.<Entry>readValues(stream).readAll();
-		} catch (IOException e) {
-			throw new RuntimeIOException(e);
-		}
-	}
-
 	@Override
 	public ListSystem getSystem() {
 		return ListSystem.create();
