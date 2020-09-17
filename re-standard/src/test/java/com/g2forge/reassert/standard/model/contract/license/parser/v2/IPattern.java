282542a8cd4011db6acea1fd63a0006bcf35ea1c
Greg Gibeling <gdgib@outlook.com>
2020-09-16 20:39:59 -0700

    G2-931 Move regex API to alexandria

--- re-standard/src/test/java/com/g2forge/reassert/standard/model/contract/license/parser/v2/IPattern.java
+++
@@ -1,7 +0,0 @@
-package com.g2forge.reassert.standard.model.contract.license.parser.v2;
-
-import com.g2forge.alexandria.java.fluent.optional.IOptional;
-
-public interface IPattern<Result> {
-	public IOptional<Result> match(String string);
-}
