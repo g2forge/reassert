88183ca97d8b4f38ba6b44c30916ce8f361577d4
Greg Gibeling <gdgib@outlook.com>
2020-09-20 18:32:58 -0700

    G2-917 Implement new TermRelation systems

---
+++ re-contract/src/main/java/com/g2forge/reassert/contract/eval/TermRelationValueSystem.java
@@ -0,0 +1,39 @@
+package com.g2forge.reassert.contract.eval;
+
+import java.util.Objects;
+
+import com.g2forge.alexandria.java.core.marker.ISingleton;
+import com.g2forge.enigma.backend.convert.textual.ITextualRenderer;
+import com.g2forge.enigma.backend.convert.textual.ToStringTextualRenderer;
+import com.g2forge.reassert.core.model.contract.terms.TermRelation;
+import com.g2forge.reassert.express.v2.eval.value.IValueSystem;
+
+public class TermRelationValueSystem implements IValueSystem<TermRelation>, ISingleton {
+	private static final TermRelationValueSystem INSTANCE = new TermRelationValueSystem();
+
+	public static TermRelationValueSystem create() {
+		return INSTANCE;
+	}
+
+	protected TermRelationValueSystem() {}
+
+	@Override
+	public ITextualRenderer<? super TermRelation> getRenderer() {
+		return ToStringTextualRenderer.create();
+	}
+
+	@Override
+	public boolean isEqual(TermRelation left, TermRelation right) {
+		return Objects.equals(left, right);
+	}
+
+	@Override
+	public boolean isSame(TermRelation left, TermRelation right) {
+		return Objects.equals(left, right);
+	}
+
+	@Override
+	public boolean isValid(TermRelation value) {
+		return value != null;
+	}
+}
