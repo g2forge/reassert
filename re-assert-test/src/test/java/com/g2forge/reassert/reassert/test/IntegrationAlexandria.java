af9d94a912879f15f049476189c523dd1e92a504
Greg Gibeling <gdgib@outlook.com>
2021-12-27 08:25:05 -0800

    G2-1275 Release alexandria 0.0.18

--- re-assert-test/src/test/java/com/g2forge/reassert/reassert/test/IntegrationAlexandria.java
+++ re-assert-test/src/test/java/com/g2forge/reassert/reassert/test/IntegrationAlexandria.java
@@ -23,7 +23,7 @@ public class IntegrationAlexandria {
 	private static final TestGraph testGraph = computeTestGraph();
 
 	protected static TestGraph computeTestGraph() {
-		return new TestGraph(new Artifact<>(null, new GitCoordinates(null, "https://github.com/g2forge/alexandria.git", "1dd72e04251f48035cc0b22c90d54638591bc937")), HCollection.emptyList());
+		return new TestGraph(new Artifact<>(null, new GitCoordinates(null, "https://github.com/g2forge/alexandria.git", "0.0.18")), HCollection.emptyList());
 	}
 
 	@Test
