c927a4fd27522e5587dd024a88c384427245ef21
Greg Gibeling <gdgib@outlook.com>
2024-07-22 14:30:41 -0700

    Created reassert test project

---
+++ src/main/java/com/g2forge/reassert/test/ReassertTest.java
@@ -0,0 +1,7 @@
+package com.g2forge.reassert.test;
+
+public class ReassertTest {
+	public static void main(String[] args) {
+		System.out.println("This is a hello world style program used by reassert for testing.");
+	}
+}
